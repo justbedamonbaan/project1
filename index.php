54bc0ba18295bc22d2e97ce58e6085c48d3c092d
justbedamonbaan <damonbaan@gmail.com>
2020-09-09 11:23:10 +0200

    login files zonder database

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html>
+<body>
+
+<form action="login.php" method="post">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <div class="imgcontainer">-->
+        <img src="empty.png" alt="Avatar" class="avatar">
+           </div>
+    Username: <input type="text" placeholder="Enter Username" name="Username"required><br>
+    Password: <input type="password" placeholder="Enter password" name="password" required><br>
+    <a href="signup.php">Create account</a>
+    <a href="lostpsw.php">Forget password?</a>
+    <br>
+    <input value="Login" type="submit">
+</form>
+</body>
+</html>
---
+++ lostpsw.php
@@ -0,0 +1,10 @@
+
+<html>
+<body>
+
+<form action="login.php" method="post">
+    E-mail: <input type="text" name="E-mail"><br>
+    <input value="Lost password" type="submit">
+</form>
+</body>
+</html>
