a20b2edae5b632439a093165b51d69bc846e410e
justbedamonbaan <damonbaan@gmail.com>
2020-09-10 10:34:11 +0200

    alleen de sql file

---
+++ sql_statements.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE project1;
+
+CREATE TABLE account ( ID int NOT NULL AUTO_INCREMENT, email VARCHAR(30) UNIQUE, password VARCHAR(30), PRIMARY KEY (ID) );
+
+volgende table
+
+CREATE TABLE persoon (
+    ID int NOT NULL,
+    voornaam varchar(255),
+    tussenvoegsel varchar(30),
+    achternaam varchar(255),
+    gebruikersnaam varchar(255) UNIQUE,
+    account_ID INT, FOREIGN KEY (account_ID) REFERENCES account(ID)
+    );
+    
